8b9b85ae4a524d55c876d207190ea41b211f260e
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-05-12 17:40:41 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Write a program that imports the variable a from the file variable_load_5.py and prints its value."""
+    from variable_load_5 import a
+
+print(a)
