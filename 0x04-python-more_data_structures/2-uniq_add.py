a3b749ac4500e832c59cafdab0fd3e520c5291e0
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-05-18 16:12:22 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """function that adds all unique integers in a list"""
+    unique_integers = set(my_list)
+    # Sum up the unique integers
+    total = sum(unique_integers)
+    
+    return total
+
