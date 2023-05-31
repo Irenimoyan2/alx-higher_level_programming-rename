0bc411b28019ee9a7f03d684300653c81f256679
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-05-31 05:04:36 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@
 import sys
 
 def safe_function(fct, *args):
-    """Executes a function safely.
+    """Function that executes a function safely.
     Args:
         fct: The function to execute.
         args: Arguments for fct.
